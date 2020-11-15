7f13af78a277f9e1f14f3ffa3c54d12e0e356d2a
ljsmalbil <louksmalbil@gmail.com>
2020-11-15 17:05:55 +0100

    var historical method added

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import numpy as np
+
 from var import ValueAtRisk
 
 if __name__ == "__main__":
+    period = "5y"
 
-    sp = ValueAtRisk()
-    print(sp.return_distribution_num())
+    sp = ValueAtRisk(period=period)
 
+    # Obtain a visual of the return for the given time period
     sp.return_distribution_visual()
+
+    # Get the VaR
+    var = sp.value_at_risk()
+    print('For a 95% CI, the VaR is: ' + str(np.round(var,3)) + ', indicating that with 95% confidence we can be sure that the daily returns will not exceed ' + str(np.round(var * 100,2)) + '%.')
+
+
 
--- var.py
+++ var.py
@@ -12,17 +12,18 @@ class ValueAtRisk:
 
     def return_distribution_num(self):
         # Get S&P500 Index data
-        market = yf.Ticker(self.default)
-        market_data = market.history(period=self.period)
+        index_data = yf.Ticker(self.default)
+        index_data = index_data.history(period=self.period)
 
         # Convert to returns and remove nan columns
-        market_data = market_data.pct_change(1)
-        market_data = market_data['Close']
-        market_data = market_data.dropna(axis=0)
+        index_data = index_data.pct_change(1)
+        index_data = index_data['Close']
+        index_data = index_data.dropna(axis=0)
 
-        return market_data
+        return index_data
 
     def return_distribution_visual(self):
+        """
         # Get S&P500 Index data
         market = yf.Ticker(self.default)
         market_data = market.history(period=self.period)
@@ -31,11 +32,28 @@ class ValueAtRisk:
         market_data = market_data.pct_change(1)
         market_data = market_data['Close']
         market_data = market_data.dropna(axis=0)
+        """
 
-        sns.histplot(market_data)
+        current_portfolio = ValueAtRisk(default=self.default, period=self.period)
+        index_data = current_portfolio.return_distribution_num()
+
+        sns.histplot(index_data)
         plt.title('S&P500 returns')
         plt.ylabel('Frequency')
         plt.xlabel('Returns (in %)')
 
         return plt.show()
+
+    def value_at_risk(self, percentile = 0.05):
+        """
+        N.B. This VaR computation uses the historical method
+
+        :param percentile: VaR percentile
+        :return:
+        """
+
+        current_portfolio = ValueAtRisk(default=self.default, period=self.period)
+        index_data = current_portfolio.return_distribution_num()
+
+        return np.quantile(index_data, percentile)
 
