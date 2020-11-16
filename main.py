8dea2bc73f723092f1b849994a086d8af0b389f6
ljsmalbil <louksmalbil@gmail.com>
2020-11-16 21:57:15 +0100

    monte carlo added

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import numpy as np
+"""
+Developed by: L. Smalbil
 
+This script can be used to compute the value at risk (VaR) of a given portfolio.
+Three methods have been implemented:
+
+1) Historical method;
+2) Variance-covariance method;
+3) Monte Carlo method (to be implemented).
+"""
+
+import numpy as np
 from var import ValueAtRisk
+
 
 if __name__ == "__main__":
     period = "5y"
@@ -8,15 +19,21 @@ if __name__ == "__main__":
     sp = ValueAtRisk(period=period)
 
     # Obtain a visual of the return for the given time period
-    sp.return_distribution_visual()
+    #sp.return_distribution_visual()
 
     # Get the VaR
-    percentile = 0.01
-    var = sp.value_at_risk(percentile = percentile)
+    percentile = 0.01 # CI
+    var = sp.value_at_risk_hist(percentile = percentile)
     print('For a ' + str((1 - percentile) * 100)+ '% CI, the VaR is: ' + str(np.round(var,3)) +
-          ', indicating that with ' + str((1 - percentile) * 100)+
+          ', indicating that with ' + str((1 - percentile) * 100) +
           '% confidence we can be sure that the daily losses will not exceed '
           + str(abs(np.round(var * 100,2))) + '%.')
 
+    var_cov = sp.value_at_risk_var_covar(percentile = percentile)
+    print('\nAccording to the variance-covariance method, the VaR for a ' + str((1 - percentile) * 100)+ '% CI' +
+          ' is ' + str(abs(np.round(var_cov * 100,2))) + '%.')
 
+    var_monte_carlo = sp.value_at_risk_monte_carlo(repetitions = 1000, percentile=percentile)
+    print('\nAccording to the Monte Carlo method, the VaR for a ' + str((1 - percentile) * 100) + '% CI' +
+          ' is ' + str(abs(np.round(var_monte_carlo * 100, 2))) + '%.')
 
--- var.py
+++ var.py
@@ -5,6 +5,9 @@ import yfinance as yf
 
 import matplotlib.pyplot as plt
 
+from scipy.stats import norm
+from scipy.stats import t
+
 class ValueAtRisk:
     def __init__(self, default = '^GSPC', period = "1y"):
         self.default = default
@@ -44,16 +47,57 @@ class ValueAtRisk:
 
         return plt.show()
 
-    def value_at_risk(self, percentile = 0.05):
+    def value_at_risk_hist(self, percentile = 0.05):
         """
         N.B. This VaR computation uses the historical method
 
-        :param percentile: VaR percentile
-        :return:
+        :param percentile: VaR percentile, CI
+        :return: VaR value
         """
 
         current_portfolio = ValueAtRisk(default=self.default, period=self.period)
         index_data = current_portfolio.return_distribution_num()
 
         return np.quantile(index_data, percentile)
+
+    def value_at_risk_var_covar(self, percentile = 0.05):
+        """
+        This VaR computation uses the variance-covariance method
+
+        N.B. A t-score is used instead of a z-score to account for uncertainties in smaller data sets.
+
+        :param percentile: VaR percentile, CI
+        :return: VaR value
+        """
+
+        current_portfolio = ValueAtRisk(default=self.default, period=self.period)
+        index_data = current_portfolio.return_distribution_num()
+
+        alpha = t.ppf(1 - percentile, df = len(index_data))
+        var_vc = alpha * np.std(index_data)
+
+        return var_vc
+
+    def value_at_risk_monte_carlo(self, repetitions = 1000, percentile = 0.05):
+        """
+        This VaR computation uses the Monte Carlo method
+
+        N.B. This method assumes a underlying Student's t-distribution.
+
+        :param repetitions: number of sample points drawn, sample size
+        :param percentile: VaR percentile, CI
+        :return:
+        """
+        current_portfolio = ValueAtRisk(default=self.default, period=self.period)
+
+        index_data = current_portfolio.return_distribution_num()
+        mu, sigma = np.mean(index_data), np.std(index_data)  # mean and standard deviation
+        monte_carlo = np.random.normal(mu, sigma, repetitions)
+
+        alpha = t.ppf(1 - percentile, df=len(monte_carlo))
+        var_mc = alpha * np.std(monte_carlo)
+
+        return var_mc
+
+
 
